bc27f85e3bafc49ede76d49a1a7fc41ed1c41b72
Soumyaranjanrrath <soumya2005rath@gmail.com>
2025-07-10 21:13:39 +0530

    Styling index

--- init/index.js
+++ init/index.js
@@ -1,28 +1,33 @@
-const mongoose = require('mongoose');
-const initData = require('./data.js');
-const Listing = require('../models/listing.js');
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/WanderInn";
 
 main()
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch(err => {
-        console.error( err);
-    });
-    
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 async function main() {
-    await mongoose.connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("Database initialized with sample data");
-}
+  await Listing.deleteMany({});
+
+  const modifiedData = initData.data.map((listing) => {
+    return {
+      ...listing,
+      image: listing.image.url, //  Important fix
+    };
+  });
+
+  await Listing.insertMany(modifiedData);
+  console.log(" Data was initialized with correct image URLs");
+};
 
 initDB();
